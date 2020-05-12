1e6d65af8277c248baa185a5b0eee18220e3b44d
NaorAnhaisy <naor.anhaisy123@gmail.com>
2020-05-12 20:14:55 +0300

    fix

--- src/StoreDB/WineStore.js
+++ src/StoreDB/WineStore.js
@@ -6,37 +6,37 @@ export function getWine() {
             name: "יין אדום מסדרת Private Collection (מרלו, שיראז, קברנה סוביניון) - יקבי כרמל",
             price: 70,
             isHot: false,
-            imageUrl: "2https://www.winesisrael.com//wp-content/uploads/%D7%A1%D7%93%D7%A8%D7%AA-%D7%A4%D7%A8%D7%99%D7%99%D7%91%D7%98-%D7%A7%D7%95%D7%9C%D7%A7%D7%A9%D7%99%D7%99%D7%9F.jpg"
+            imageUrl: "https://www.winesisrael.com//wp-content/uploads/%D7%A1%D7%93%D7%A8%D7%AA-%D7%A4%D7%A8%D7%99%D7%99%D7%91%D7%98-%D7%A7%D7%95%D7%9C%D7%A7%D7%A9%D7%99%D7%99%D7%9F.jpg"
         },
         {
             name: "יין לבן מסדרת Private Collection (שרדונה, אמרלד ריזלינג קולומברד) - יקבי כרמל",
             price: 70,
             isHot: false,
-            imageUrl: "2https://cdn.exiteme.com/exitetogo/www.wine-and-spirits.co.il/userfiles/images/wine/%D7%A4%D7%A8%D7%99%D7%99%D7%91%D7%98%20%D7%A7%D7%95%D7%9C%D7%A7%D7%A9%D7%9F%20%D7%9C%D7%A9%D7%91%D7%95%D7%A2%D7%95%D7%AA%20%D7%AA%D7%9E%D7%95%D7%A0%D7%AA%20%D7%AA%D7%95%D7%9B%D7%9F.jpg"
+            imageUrl: "https://cdn.exiteme.com/exitetogo/www.wine-and-spirits.co.il/userfiles/images/wine/%D7%A4%D7%A8%D7%99%D7%99%D7%91%D7%98%20%D7%A7%D7%95%D7%9C%D7%A7%D7%A9%D7%9F%20%D7%9C%D7%A9%D7%91%D7%95%D7%A2%D7%95%D7%AA%20%D7%AA%D7%9E%D7%95%D7%A0%D7%AA%20%D7%AA%D7%95%D7%9B%D7%9F.jpg"
         },
         {
             name: "יין אדום יבש מסדרת Selected - יקבי כרמל",
             price: 100,
             isHot: false,
-            imageUrl: "2https://i.ibb.co/NtKrpkz/selected.png"
+            imageUrl: "https://i.ibb.co/NtKrpkz/selected.png"
         },
         {
             name: "תירוש כרמל לקידוש",
             price: 210,
             isHot: false,
-            imageUrl: "2https://carmelwines.co.il/wp-content/uploads/2018/03/Untitled-3.jpg"
+            imageUrl: "https://carmelwines.co.il/wp-content/uploads/2018/03/Untitled-3.jpg"
         },
         {
             name: "יין מסדרת Buzz - יקבי כרמל",
             price: 210,
             isHot: false,
-            imageUrl: "2https://www.winesisrael.com//wp-content/uploads/%D7%99%D7%A7%D7%91%D7%99-%D7%9B%D7%A8%D7%9E%D7%9C_%D7%A1%D7%93%D7%A8%D7%AA-%D7%99%D7%99%D7%A0%D7%95%D7%AA-BUZZ_%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%A1%D7%9C%D7%99-%D7%A4%D7%98%D7%9C-3.jpg"
+            imageUrl: "https://www.winesisrael.com//wp-content/uploads/%D7%99%D7%A7%D7%91%D7%99-%D7%9B%D7%A8%D7%9E%D7%9C_%D7%A1%D7%93%D7%A8%D7%AA-%D7%99%D7%99%D7%A0%D7%95%D7%AA-BUZZ_%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%A1%D7%9C%D7%99-%D7%A4%D7%98%D7%9C-3.jpg"
         },
         {
             name: "יין קינג דיוויד - יקבי כרמל",
             price: 210,
             isHot: false,
-            imageUrl: "2https://img.zap.co.il/pics/8/1/1/2/46132118c.gif"
+            imageUrl: "https://img.zap.co.il/pics/8/1/1/2/46132118c.gif"
         },
         {
             name: "יין אדום יבש - Vino",
